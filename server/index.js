2533aa274d464dbb1c20f4dbefeb8c6abbfe5605
pszafo <ps@zafo.in>
2025-02-27 12:46:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const searchRouter = require('./routes/search');
+const dashboardRouter = require('./routes/dashboard');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/search', searchRouter);
+app.use('/api/dashboard-data', dashboardRouter);
 
 app.get('/', (req, res) => {
   res.send('Zafo.ai Analytics Tool Backend');
