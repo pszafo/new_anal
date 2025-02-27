96a15f56b75bab7d0e6a32f8a2e1fc511d62287b
pszafo <ps@zafo.in>
2025-02-27 12:54:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const searchRouter = require('./routes/search');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const dashboardRouter = require('./routes/dashboard');
 
 const app = express();
@@ -7,6 +9,8 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
   // app.use('/api/search', searchRouter);
+app.use(cors());
+app.use(bodyParser.json());
 app.use('/api', dashboardRouter);
 
 app.get('/', (req, res) => {
